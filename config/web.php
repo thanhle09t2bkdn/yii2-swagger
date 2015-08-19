73074c7bd3e7600f8e4674f28078268047ea83de
thanhle <le_bakhanhthanh@co-mit.com.vn>
2015-08-19 13:47:20 +0700

    fix url

--- config/web.php
+++ config/web.php
@@ -7,6 +7,12 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],        
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'unlUhRabDYW06YPEe_sBfqpR_Nb7I4a7',
